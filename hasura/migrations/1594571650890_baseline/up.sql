e688d8e5ba624cc7b73fa46faf833bb060e199a4
Raumo0 <dharapko@gmail.com>
2020-07-21 13:22:04 +0300

    column renaming

--- hasura/migrations/1594571650890_baseline/up.sql
+++ hasura/migrations/1594571650890_baseline/up.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA likelib;
 CREATE TABLE likelib.account_types (
-    value character varying(255) NOT NULL
+    type character varying(255) NOT NULL
 );
 CREATE TABLE likelib.accounts (
     address character varying(255) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE likelib.accounts (
     type character varying(255) NOT NULL
 );
 CREATE TABLE likelib.blocks (
-    number bigint NOT NULL,
+    height bigint NOT NULL,
     hash character varying(255) NOT NULL,
     hash_in_base64 character varying(255) NOT NULL,
     "timestamp" timestamp without time zone NOT NULL,
@@ -20,19 +20,19 @@ CREATE TABLE likelib.blocks (
     block_data_in_json character varying(5000) NOT NULL
 );
 CREATE TABLE likelib.transaction_statuses (
-    value character varying(255) NOT NULL
+    status character varying(255) NOT NULL
 );
 CREATE TABLE likelib.transaction_types (
-    value character varying(255) NOT NULL
+    type character varying(255) NOT NULL
 );
 CREATE TABLE likelib.transactions (
     hash character varying(255) NOT NULL,
     hash_in_base64 character varying(255) NOT NULL,
     "timestamp" timestamp without time zone NOT NULL,
-    address_to character varying(255) NOT NULL,
-    address_to_in_base58 character varying(255) NOT NULL,
-    address_from character varying(255) NOT NULL,
-    address_from_in_base58 character varying(255) NOT NULL,
+    account_to character varying(255) NOT NULL,
+    account_to_in_base58 character varying(255) NOT NULL,
+    account_from character varying(255) NOT NULL,
+    account_from_in_base58 character varying(255) NOT NULL,
     data bytea,
     amount bigint NOT NULL,
     sign character varying(500) NOT NULL,
@@ -45,9 +45,9 @@ CREATE TABLE likelib.transactions (
     type character varying(255) NOT NULL
 );
 ALTER TABLE ONLY likelib.account_types
-    ADD CONSTRAINT account_types_pkey PRIMARY KEY (value);
+    ADD CONSTRAINT account_types_pkey PRIMARY KEY (type);
 ALTER TABLE ONLY likelib.account_types
-    ADD CONSTRAINT account_types_value_key UNIQUE (value);
+    ADD CONSTRAINT account_types_value_key UNIQUE (type);
 ALTER TABLE ONLY likelib.accounts
     ADD CONSTRAINT accounts_address_in_base58_key UNIQUE (address_in_base58);
 ALTER TABLE ONLY likelib.accounts
@@ -57,7 +57,7 @@ ALTER TABLE ONLY likelib.blocks
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_hash_key UNIQUE (hash);
 ALTER TABLE ONLY likelib.blocks
-    ADD CONSTRAINT blocks_pkey PRIMARY KEY (number);
+    ADD CONSTRAINT blocks_pkey PRIMARY KEY (height);
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_prev_block_hash_in_base64_key UNIQUE (prev_block_hash_in_base64);
 ALTER TABLE ONLY likelib.blocks
@@ -65,13 +65,13 @@ ALTER TABLE ONLY likelib.blocks
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_timestamp_key UNIQUE ("timestamp");
 ALTER TABLE ONLY likelib.transaction_statuses
-    ADD CONSTRAINT transaction_statuses_pkey PRIMARY KEY (value);
+    ADD CONSTRAINT transaction_statuses_pkey PRIMARY KEY (status);
 ALTER TABLE ONLY likelib.transaction_statuses
-    ADD CONSTRAINT transaction_statuses_value_key UNIQUE (value);
+    ADD CONSTRAINT transaction_statuses_value_key UNIQUE (status);
 ALTER TABLE ONLY likelib.transaction_types
-    ADD CONSTRAINT transaction_types_pkey PRIMARY KEY (value);
+    ADD CONSTRAINT transaction_types_pkey PRIMARY KEY (type);
 ALTER TABLE ONLY likelib.transaction_types
-    ADD CONSTRAINT transaction_types_value_key UNIQUE (value);
+    ADD CONSTRAINT transaction_types_value_key UNIQUE (type);
 ALTER TABLE ONLY likelib.transactions
     ADD CONSTRAINT transactions_hash_in_base64_key UNIQUE (hash_in_base64);
 ALTER TABLE ONLY likelib.transactions
@@ -84,14 +84,14 @@ CREATE INDEX accounts_type_idx ON likelib.accounts USING btree (type);
 CREATE INDEX blocks_hash_idx ON likelib.blocks USING btree (hash);
 CREATE INDEX blocks_hash_in_base64_idx ON likelib.blocks USING btree (hash_in_base64);
 CREATE INDEX blocks_nonce_idx ON likelib.blocks USING btree (nonce);
-CREATE INDEX blocks_number_idx ON likelib.blocks USING btree (number);
+CREATE INDEX blocks_number_idx ON likelib.blocks USING btree (height);
 CREATE INDEX blocks_prev_block_hash_idx ON likelib.blocks USING btree (prev_block_hash);
 CREATE INDEX blocks_prev_block_hash_in_base64_idx ON likelib.blocks USING btree (prev_block_hash_in_base64);
 CREATE INDEX blocks_timestamp_idx ON likelib.blocks USING btree ("timestamp");
-CREATE INDEX transactions_address_from_idx ON likelib.transactions USING btree (address_from);
-CREATE INDEX transactions_address_from_in_base58_idx ON likelib.transactions USING btree (address_from_in_base58);
-CREATE INDEX transactions_address_to_idx ON likelib.transactions USING btree (address_to);
-CREATE INDEX transactions_address_to_in_base58_idx ON likelib.transactions USING btree (address_to_in_base58);
+CREATE INDEX transactions_address_from_idx ON likelib.transactions USING btree (account_from);
+CREATE INDEX transactions_address_from_in_base58_idx ON likelib.transactions USING btree (account_from_in_base58);
+CREATE INDEX transactions_address_to_idx ON likelib.transactions USING btree (account_to);
+CREATE INDEX transactions_address_to_in_base58_idx ON likelib.transactions USING btree (account_to_in_base58);
 CREATE INDEX transactions_amount_idx ON likelib.transactions USING btree (amount);
 CREATE INDEX transactions_block_height_idx ON likelib.transactions USING btree (block_height);
 CREATE INDEX transactions_fee_idx ON likelib.transactions USING btree (fee);
@@ -101,14 +101,14 @@ CREATE INDEX transactions_status_idx ON likelib.transactions USING btree (status
 CREATE INDEX transactions_timestamp_idx ON likelib.transactions USING btree ("timestamp");
 CREATE INDEX transactions_type_idx ON likelib.transactions USING btree (type);
 ALTER TABLE ONLY likelib.accounts
-    ADD CONSTRAINT accounts_type_fkey FOREIGN KEY (type) REFERENCES likelib.account_types(value) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT accounts_type_fkey FOREIGN KEY (type) REFERENCES likelib.account_types(type) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_address_from_fkey FOREIGN KEY (address_from) REFERENCES likelib.accounts(address);
+    ADD CONSTRAINT transactions_address_from_fkey FOREIGN KEY (account_from) REFERENCES likelib.accounts(address);
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_address_to_fkey FOREIGN KEY (address_to) REFERENCES likelib.accounts(address);
+    ADD CONSTRAINT transactions_address_to_fkey FOREIGN KEY (account_to) REFERENCES likelib.accounts(address);
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_block_height_fkey FOREIGN KEY (block_height) REFERENCES likelib.blocks(number) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT transactions_block_height_fkey FOREIGN KEY (block_height) REFERENCES likelib.blocks(height) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_status_fkey FOREIGN KEY (status) REFERENCES likelib.transaction_statuses(value) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT transactions_status_fkey FOREIGN KEY (status) REFERENCES likelib.transaction_statuses(status) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_type_fkey FOREIGN KEY (type) REFERENCES likelib.transaction_types(value) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT transactions_type_fkey FOREIGN KEY (type) REFERENCES likelib.transaction_types(type) ON UPDATE RESTRICT ON DELETE RESTRICT;
