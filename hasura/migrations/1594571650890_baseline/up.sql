2466485b838a367288caf4838f32a64a150155d3
Raumo0 <dharapko@gmail.com>
2020-07-21 07:34:41 +0300

    hasura migration created

--- hasura/migrations/1594571650890_baseline/up.sql
+++ hasura/migrations/1594571650890_baseline/up.sql
@@ -1,115 +1,96 @@
 CREATE SCHEMA likelib;
-
-create table likelib.transaction_statuses (
-    key                         int             not null,
-    value                       varchar(255)    not null,
-    primary key (key)
+CREATE TABLE likelib.account_types (
+    key integer NOT NULL,
+    value character varying(255) NOT NULL
 );
-
-insert into likelib.transaction_statuses(key, value) values (0, 'Success');
-insert into likelib.transaction_statuses(key, value) values (1, 'Pending');
-insert into likelib.transaction_statuses(key, value) values (2, 'BadQueryForm');
-insert into likelib.transaction_statuses(key, value) values (3, 'BadSign');
-insert into likelib.transaction_statuses(key, value) values (4, 'NotEnoughBalance');
-insert into likelib.transaction_statuses(key, value) values (5, 'Revert');
-insert into likelib.transaction_statuses(key, value) values (6, 'Failed');
-
-create table likelib.transaction_types (
-    key                         int             not null,
-    value                       varchar(255)    not null,
-    primary key (key)
+CREATE TABLE likelib.accounts (
+    address character varying(255) NOT NULL,
+    address_in_base58 character varying(255) NOT NULL,
+    balance bigint,
+    nonce integer,
+    type integer
 );
-
-insert into likelib.transaction_types(key, value) values (0, 'None');
-insert into likelib.transaction_types(key, value) values (1, 'Transfer');
-insert into likelib.transaction_types(key, value) values (2, 'ContractCall');
-insert into likelib.transaction_types(key, value) values (3, 'ContractCreation');
-
-
-
-create table likelib.account_types (
-    key                         int             not null,
-    value                       varchar(255)    not null,
-    primary key (key)
+CREATE TABLE likelib.blocks (
+    number bigint NOT NULL,
+    hash character varying(255) NOT NULL,
+    hash_in_base64 character varying(255) NOT NULL,
+    "timestamp" timestamp without time zone,
+    nonce integer,
+    prev_block_hash character varying(255),
+    prev_block_hash_in_base64 character varying(255),
+    block_data_in_json character varying(5000) NOT NULL
 );
-
-insert into likelib.account_types(key, value) values (0, 'Client');
-insert into likelib.account_types(key, value) values (1, 'Contract');
-
-
-
-
-create table likelib.blocks (
-    number                      bigint          not null,
-    hash                        varchar(255)    not null,
-    hash_in_base64              varchar(255)    not null,
-    timestamp                   timestamp,
-    nonce                       int,
-    prev_block_hash             varchar(255),
-    prev_block_hash_in_base64   varchar(255),
-    block_data_in_json          varchar(5000)   not null,
-    primary key (number)
+CREATE TABLE likelib.transaction_statuses (
+    key integer NOT NULL,
+    value character varying(255) NOT NULL
 );
-
-create table likelib.accounts (
-    address                     varchar(255)    not null,
-    address_in_base58           varchar(255)    not null,
-    balance                     bigint,
-    nonce                       int,
-    type                        int,
-    primary key (address),
-    foreign key (type) references likelib.account_types(key)
+CREATE TABLE likelib.transaction_types (
+    key integer NOT NULL,
+    value character varying(255) NOT NULL
 );
-
-create table likelib.transactions (
-    hash                        varchar(255)    not null,
-    hash_in_base64              varchar(255)    not null,
-    timestamp                   timestamp,
-    address_to                  varchar(255),
-    address_to_in_base58        varchar(255),
-    address_from                varchar(255),
-    address_from_in_base58      varchar(255),
-    data                        bytea,
-    amount                      bigint,
-    status                      int,
-    sign                        varchar(500),
-    sign_in_base64              varchar(500),
-    fee                         bigint,
-    block_height                bigint,
-    type                        int,
-    message                     varchar(500),
-    message_in_base64           varchar(500),
-    primary key (hash),
-    foreign key (address_to) references likelib.accounts(address),
-    foreign key (address_from) references likelib.accounts(address),
-    foreign key (status) references likelib.transaction_statuses(key),
-    foreign key (block_height) references likelib.blocks(number),
-    foreign key (type) references likelib.transaction_types(key)
+CREATE TABLE likelib.transactions (
+    hash character varying(255) NOT NULL,
+    hash_in_base64 character varying(255) NOT NULL,
+    "timestamp" timestamp without time zone,
+    address_to character varying(255),
+    address_to_in_base58 character varying(255),
+    address_from character varying(255),
+    address_from_in_base58 character varying(255),
+    data bytea,
+    amount bigint,
+    status integer,
+    sign character varying(500),
+    sign_in_base64 character varying(500),
+    fee bigint,
+    block_height bigint,
+    type integer,
+    message character varying(500),
+    message_in_base64 character varying(500)
 );
-
-create index on likelib.blocks (number);
-create index on likelib.blocks (hash);
-create index on likelib.blocks (hash_in_base64);
-create index on likelib.blocks (timestamp);
-create index on likelib.blocks (nonce);
-create index on likelib.blocks (prev_block_hash);
-create index on likelib.blocks (prev_block_hash_in_base64);
-
-create index on likelib.transactions (hash);
-create index on likelib.transactions (hash_in_base64);
-create index on likelib.transactions (timestamp);
-create index on likelib.transactions (address_to);
-create index on likelib.transactions (address_to_in_base58);
-create index on likelib.transactions (address_from);
-create index on likelib.transactions (address_from_in_base58);
-create index on likelib.transactions (amount);
-create index on likelib.transactions (status);
-create index on likelib.transactions (fee);
-create index on likelib.transactions (block_height);
-
-create index on likelib.accounts (address);
-create index on likelib.accounts (address_in_base58);
-create index on likelib.accounts (balance);
-create index on likelib.accounts (nonce);
-create index on likelib.accounts (type);
-
+ALTER TABLE ONLY likelib.account_types
+    ADD CONSTRAINT account_types_pkey PRIMARY KEY (key);
+ALTER TABLE ONLY likelib.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (address);
+ALTER TABLE ONLY likelib.blocks
+    ADD CONSTRAINT blocks_pkey PRIMARY KEY (number);
+ALTER TABLE ONLY likelib.transaction_statuses
+    ADD CONSTRAINT transaction_statuses_pkey PRIMARY KEY (key);
+ALTER TABLE ONLY likelib.transaction_types
+    ADD CONSTRAINT transaction_types_pkey PRIMARY KEY (key);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_pkey PRIMARY KEY (hash);
+CREATE INDEX accounts_address_idx ON likelib.accounts USING btree (address);
+CREATE INDEX accounts_address_in_base58_idx ON likelib.accounts USING btree (address_in_base58);
+CREATE INDEX accounts_balance_idx ON likelib.accounts USING btree (balance);
+CREATE INDEX accounts_nonce_idx ON likelib.accounts USING btree (nonce);
+CREATE INDEX accounts_type_idx ON likelib.accounts USING btree (type);
+CREATE INDEX blocks_hash_idx ON likelib.blocks USING btree (hash);
+CREATE INDEX blocks_hash_in_base64_idx ON likelib.blocks USING btree (hash_in_base64);
+CREATE INDEX blocks_nonce_idx ON likelib.blocks USING btree (nonce);
+CREATE INDEX blocks_number_idx ON likelib.blocks USING btree (number);
+CREATE INDEX blocks_prev_block_hash_idx ON likelib.blocks USING btree (prev_block_hash);
+CREATE INDEX blocks_prev_block_hash_in_base64_idx ON likelib.blocks USING btree (prev_block_hash_in_base64);
+CREATE INDEX blocks_timestamp_idx ON likelib.blocks USING btree ("timestamp");
+CREATE INDEX transactions_address_from_idx ON likelib.transactions USING btree (address_from);
+CREATE INDEX transactions_address_from_in_base58_idx ON likelib.transactions USING btree (address_from_in_base58);
+CREATE INDEX transactions_address_to_idx ON likelib.transactions USING btree (address_to);
+CREATE INDEX transactions_address_to_in_base58_idx ON likelib.transactions USING btree (address_to_in_base58);
+CREATE INDEX transactions_amount_idx ON likelib.transactions USING btree (amount);
+CREATE INDEX transactions_block_height_idx ON likelib.transactions USING btree (block_height);
+CREATE INDEX transactions_fee_idx ON likelib.transactions USING btree (fee);
+CREATE INDEX transactions_hash_idx ON likelib.transactions USING btree (hash);
+CREATE INDEX transactions_hash_in_base64_idx ON likelib.transactions USING btree (hash_in_base64);
+CREATE INDEX transactions_status_idx ON likelib.transactions USING btree (status);
+CREATE INDEX transactions_timestamp_idx ON likelib.transactions USING btree ("timestamp");
+ALTER TABLE ONLY likelib.accounts
+    ADD CONSTRAINT accounts_type_fkey FOREIGN KEY (type) REFERENCES likelib.account_types(key);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_address_from_fkey FOREIGN KEY (address_from) REFERENCES likelib.accounts(address);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_address_to_fkey FOREIGN KEY (address_to) REFERENCES likelib.accounts(address);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_block_height_fkey FOREIGN KEY (block_height) REFERENCES likelib.blocks(number);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_status_fkey FOREIGN KEY (status) REFERENCES likelib.transaction_statuses(key);
+ALTER TABLE ONLY likelib.transactions
+    ADD CONSTRAINT transactions_type_fkey FOREIGN KEY (type) REFERENCES likelib.transaction_types(key);
