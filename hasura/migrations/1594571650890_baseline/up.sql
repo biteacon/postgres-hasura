f05e68bf6b08253f84cbf4c38db15aea21748244
Raumo0 <dharapko@gmail.com>
2020-07-21 17:47:29 +0300

    Extra columns removed

--- hasura/migrations/1594571650890_baseline/up.sql
+++ hasura/migrations/1594571650890_baseline/up.sql
@@ -4,7 +4,6 @@ CREATE TABLE likelib.account_types (
 );
 CREATE TABLE likelib.accounts (
     address character varying(255) NOT NULL,
-    address_in_base58 character varying(255) NOT NULL,
     balance bigint NOT NULL,
     nonce integer NOT NULL,
     type character varying(255) NOT NULL
@@ -12,11 +11,9 @@ CREATE TABLE likelib.accounts (
 CREATE TABLE likelib.blocks (
     height bigint NOT NULL,
     hash character varying(255) NOT NULL,
-    hash_in_base64 character varying(255) NOT NULL,
     "timestamp" timestamp without time zone NOT NULL,
     nonce integer NOT NULL,
-    prev_block_hash character varying(255) NOT NULL,
-    prev_block_hash_in_base64 character varying(255) NOT NULL
+    prev_block_hash character varying(255) NOT NULL
 );
 CREATE TABLE likelib.transaction_statuses (
     status character varying(255) NOT NULL
@@ -26,20 +23,15 @@ CREATE TABLE likelib.transaction_types (
 );
 CREATE TABLE likelib.transactions (
     hash character varying(255) NOT NULL,
-    hash_in_base64 character varying(255) NOT NULL,
     "timestamp" timestamp without time zone NOT NULL,
     account_to character varying(255) NOT NULL,
-    account_to_in_base58 character varying(255) NOT NULL,
     account_from character varying(255) NOT NULL,
-    account_from_in_base58 character varying(255) NOT NULL,
     data bytea,
     amount bigint NOT NULL,
     sign character varying(500) NOT NULL,
-    sign_in_base64 character varying(500) NOT NULL,
     fee bigint NOT NULL,
     block_height bigint NOT NULL,
     message character varying(500),
-    message_in_base64 character varying(500),
     status character varying(255) NOT NULL,
     type character varying(255) NOT NULL
 );
@@ -48,19 +40,13 @@ ALTER TABLE ONLY likelib.account_types
 ALTER TABLE ONLY likelib.account_types
     ADD CONSTRAINT account_types_value_key UNIQUE (type);
 ALTER TABLE ONLY likelib.accounts
-    ADD CONSTRAINT accounts_address_in_base58_key UNIQUE (address_in_base58);
-ALTER TABLE ONLY likelib.accounts
     ADD CONSTRAINT accounts_pkey PRIMARY KEY (address);
-ALTER TABLE ONLY likelib.blocks
-    ADD CONSTRAINT blocks_hash_in_base64_key UNIQUE (hash_in_base64);
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_hash_key UNIQUE (hash);
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_nonce_key UNIQUE (nonce);
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_pkey PRIMARY KEY (height);
-ALTER TABLE ONLY likelib.blocks
-    ADD CONSTRAINT blocks_prev_block_hash_in_base64_key UNIQUE (prev_block_hash_in_base64);
 ALTER TABLE ONLY likelib.blocks
     ADD CONSTRAINT blocks_prev_block_hash_key UNIQUE (prev_block_hash);
 ALTER TABLE ONLY likelib.blocks
@@ -74,30 +60,22 @@ ALTER TABLE ONLY likelib.transaction_types
 ALTER TABLE ONLY likelib.transaction_types
     ADD CONSTRAINT transaction_types_value_key UNIQUE (type);
 ALTER TABLE ONLY likelib.transactions
-    ADD CONSTRAINT transactions_hash_in_base64_key UNIQUE (hash_in_base64);
-ALTER TABLE ONLY likelib.transactions
     ADD CONSTRAINT transactions_pkey PRIMARY KEY (hash);
 CREATE INDEX accounts_address_idx ON likelib.accounts USING btree (address);
-CREATE INDEX accounts_address_in_base58_idx ON likelib.accounts USING btree (address_in_base58);
 CREATE INDEX accounts_balance_idx ON likelib.accounts USING btree (balance);
 CREATE INDEX accounts_nonce_idx ON likelib.accounts USING btree (nonce);
 CREATE INDEX accounts_type_idx ON likelib.accounts USING btree (type);
 CREATE INDEX blocks_hash_idx ON likelib.blocks USING btree (hash);
-CREATE INDEX blocks_hash_in_base64_idx ON likelib.blocks USING btree (hash_in_base64);
 CREATE INDEX blocks_nonce_idx ON likelib.blocks USING btree (nonce);
 CREATE INDEX blocks_number_idx ON likelib.blocks USING btree (height);
 CREATE INDEX blocks_prev_block_hash_idx ON likelib.blocks USING btree (prev_block_hash);
-CREATE INDEX blocks_prev_block_hash_in_base64_idx ON likelib.blocks USING btree (prev_block_hash_in_base64);
 CREATE INDEX blocks_timestamp_idx ON likelib.blocks USING btree ("timestamp");
 CREATE INDEX transactions_address_from_idx ON likelib.transactions USING btree (account_from);
-CREATE INDEX transactions_address_from_in_base58_idx ON likelib.transactions USING btree (account_from_in_base58);
 CREATE INDEX transactions_address_to_idx ON likelib.transactions USING btree (account_to);
-CREATE INDEX transactions_address_to_in_base58_idx ON likelib.transactions USING btree (account_to_in_base58);
 CREATE INDEX transactions_amount_idx ON likelib.transactions USING btree (amount);
 CREATE INDEX transactions_block_height_idx ON likelib.transactions USING btree (block_height);
 CREATE INDEX transactions_fee_idx ON likelib.transactions USING btree (fee);
 CREATE INDEX transactions_hash_idx ON likelib.transactions USING btree (hash);
-CREATE INDEX transactions_hash_in_base64_idx ON likelib.transactions USING btree (hash_in_base64);
 CREATE INDEX transactions_status_idx ON likelib.transactions USING btree (status);
 CREATE INDEX transactions_timestamp_idx ON likelib.transactions USING btree ("timestamp");
 CREATE INDEX transactions_type_idx ON likelib.transactions USING btree (type);
