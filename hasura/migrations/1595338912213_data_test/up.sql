1b15f57f25b65de2a34f9e33bb8aad586db3e2ae
Raumo0 <dharapko@gmail.com>
2020-07-23 03:38:21 +0300

    Restore data from dump for tests

---
+++ hasura/migrations/1595338912213_data_test/up.sql
@@ -0,0 +1,63 @@
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('hUoo84MPiR7qzTEmqBNftXCNsDo', 5056114, 0, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', 22822, 0, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('23FwBsXWDL6dbDMYn923mbkukoCU', 16036, 56, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('2kdCcijCmHQBQeN88SHZpW8QKVJ', 16134, 53, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('4JM4ZG7x987cSxW8KidrLsaWW3qt', 14173, 58, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('2AxxFU3W6L4eNuSsADf876cpYx7k', 10954, 50, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('3ezRvF7FrfiFwPgS8GmHkw2cAEuM', 25084, 53, 'Client');
+INSERT INTO likelib.accounts(address, balance, nonce, type) VALUES ('2rmchNFzUgJwvVtSew3P2tVckhFT', 21436, 50, 'Client');
+
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5796, '1BZAGsLN8I7/A+hZTGIY8OX+mfEBYTD3aTkbWWc/gWc=', '2020-07-19 21:16:54', 1.294047458425366e+19, 'eDG6RBS63Svt0BW6REEKgL8xkvWlgd5kcHFdwz4JEGY=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5795, 'eDG6RBS63Svt0BW6REEKgL8xkvWlgd5kcHFdwz4JEGY=', '2020-07-19 21:16:17', 5.088546827672609e+18, '/P2OvkrbPZPkE9mBUsx6GbzSw8iNxwXNM+is1By1+GQ=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5794, '/P2OvkrbPZPkE9mBUsx6GbzSw8iNxwXNM+is1By1+GQ=', '2020-07-19 21:15:32', 1.3614962156582142e+19, 'Jrq5LidE/SV5L/83dq82Re8BK9Mg+Hx4ilVn90vdUcQ=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5793, 'Jrq5LidE/SV5L/83dq82Re8BK9Mg+Hx4ilVn90vdUcQ=', '2020-07-19 21:15:31', 7.088064084865363e+18, 'sXopYtKYxe4ksXUWuaTO/5lbJPk80tffzOI9/re65hQ=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5792, 'sXopYtKYxe4ksXUWuaTO/5lbJPk80tffzOI9/re65hQ=', '2020-07-19 21:15:09', 3.403228170990772e+18, '+jxPPaxuFEKURIquEc2nlstfmtAkHk/91F5sN2qo3ZM=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5791, '+jxPPaxuFEKURIquEc2nlstfmtAkHk/91F5sN2qo3ZM=', '2020-07-19 21:15:00', 1.4797872522429389e+19, 'pLvUBMscCwy3iOti5OCZkDoSjcBZGvoPFiHHwUKneMY=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5790, 'pLvUBMscCwy3iOti5OCZkDoSjcBZGvoPFiHHwUKneMY=', '2020-07-19 21:14:52', 1.415068919612723e+19, 'x5euVXCnzt70EtbcWNHtXS2c/gjPxgZKegj66Z9E/vY=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5789, 'x5euVXCnzt70EtbcWNHtXS2c/gjPxgZKegj66Z9E/vY=', '2020-07-19 21:14:30', 1.4982219468270721e+19, '93cDIdRu64WD0BP/tGlmgBAshFd1HkWjbEQtCVLOxmk=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5788, '93cDIdRu64WD0BP/tGlmgBAshFd1HkWjbEQtCVLOxmk=', '2020-07-19 21:14:24', 1.75538995798441e+19, 'fvN81FYOcHbqn+7IbjPW5s52C4se4z+MV2FKudgoW9c=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5787, 'fvN81FYOcHbqn+7IbjPW5s52C4se4z+MV2FKudgoW9c=', '2020-07-19 21:14:11', 1.8792314527450243e+18, 'Fq9JOnN2y/eDOiRxTZd2/kE1IkLoXYC+3u7BqJ2ER8s=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+INSERT INTO likelib.blocks(height, hash, "timestamp", nonce, prev_block_hash, coinbase) 
+    VALUES (5786, 'Fq9JOnN2y/eDOiRxTZd2/kE1IkLoXYC+3u7BqJ2ER8s=', '2020-07-19 21:13:55', 1.5580813381558442e+19, 'BABnuyf18tDVSCB0Y0TlwrUqhCKbhzdQOCjv7l1xM0Q=','hUoo84MPiR7qzTEmqBNftXCNsDo');
+
+
+--
+-- Data for Name: transactions; Type: TABLE DATA; Schema: likelib; Owner: postgres
+--
+
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('3KVwjqvgoA4GeVfpRcrVbiO/Mf8ksAA03PE5PHE8lZ4=', '2020-07-19 21:16:54', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '23FwBsXWDL6dbDMYn923mbkukoCU', null, 97, 'tTiB2N5cGSoE+vaymTUiZN/N8vgzO6GwC0b0R+xnMsQSceBzVFxwsqEsfB1+A04TlZDSwx0NqTVBYtO/uer+FgE=', 0, 5796, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('AmP9BVIerls+iKrtQ8gpIqcPfKIyD6cgGnYGIB+c/e4=', '2020-07-19 21:16:17', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2kdCcijCmHQBQeN88SHZpW8QKVJ', null, 18, 'fdzT/kQX5vgFlhgP9/hiJ56BOPQB1AtHywf8W2TF+ZQooNl6XgGjNYvrQ1B2ld5rOp78Z0cqX8tPcSUdVsg9jwA=', 0, 5796, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('X/jtwU36vxk5Hg671GYqSAzTSQLpsTJZi37A26pIY4c=', '2020-07-19 21:15:32', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '4JM4ZG7x987cSxW8KidrLsaWW3qt', null, 38, '6OfdM0yvAbAbGNJ4idNXL3nf8lBapaNFChrc3Mz5aMot2EaG97A+QvrF619XGA3u0fdCHIV8mETOyE29EO+SCQE=', 0, 5795, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('ehi1MBsvY/fyNw3tx6oL+z1JWxDCb3JOiXX2oB7tsBI=', '2020-07-19 21:16:17', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '23FwBsXWDL6dbDMYn923mbkukoCU', null, 76, 'raoIm7Z+L78Weaeg15/Un4ALdrujSD/g/eEHnz1Gu8RUWTaUB+rC7K5Febj2mjX/WEbsLyYy5Ny8HDZI1bF8zgA=', 0, 5795, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('Xpqtr4BC7RDuyunN28Q9UllFxhXJjEQRMJx4LnlAnuU=', '2020-07-19 21:15:32', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2AxxFU3W6L4eNuSsADf876cpYx7k', null, 18, 'U31E2au/h9oWzTbOkCH9A2mDhvpCLGrsZxGodkMwKR8ikElUWlMTLEPMIFq+x0u2wcXl9CyXPXOZAFbpe2CFcgE=', 0, 5794, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('8tfeRRv2BMkcDjVK7O4Ox7i302GuL6HgBtFuRTtJMNg=', '2020-07-19 21:15:31', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '3ezRvF7FrfiFwPgS8GmHkw2cAEuM', null, 86, '/SkUtV0jB9bwWU9/+s0M8rzyPBT7MGCwQOg+iFwcnhcq+5nVojTJaVC4Ox+OpBopzkVfTmkCLO5Zdrz72/QZHQA=', 0, 5793, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('RfIEhq651DiPvMjw7K3+FhEYXxH0o8hXVs5oMdicmME=', '2020-07-19 21:15:09', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2AxxFU3W6L4eNuSsADf876cpYx7k', null, 87, '+wx9BDINyC0uzuL1fLvXEUsbTVSQgxvp9sCRV7MXpfIWrFIIj/4hfOdL7888dt7f+FqlXIXmC+DYNKAGCKSX9wA=', 0, 5792, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('hu6axAYUxVYm1xgq1Ec7s4JMI9AOtn0B+7UOsaRhUEk=', '2020-07-19 21:15:00', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2rmchNFzUgJwvVtSew3P2tVckhFT', null, 2, 'f3kDT3Jvqf0sidKFijsMUkHz6lD/PAlk9Dc4wEhFJwAnUwCYEd2+FmeVUdbcrF2jUVxufqumtovu6namn4PRigE=', 0, 5791, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('MEqH5cvLcuU9bEUulOyNXMB3EpCperb+krAehJPX260=', '2020-07-19 21:14:52', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '3ezRvF7FrfiFwPgS8GmHkw2cAEuM', null, 63, '4BUQ/OwADjqI29nrZ7JXLLS0KGGADm76XHkYHzwxS7MgefXOelwBgt48ej9u+OOrJWOxtD0LFvIWndXhl7+v/wA=', 0, 5790, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('ViMxi3WbCMi19vgKz1eTwFXNYA9UfoHuxWg8davN2aY=', '2020-07-19 21:14:30', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2rmchNFzUgJwvVtSew3P2tVckhFT', null, 4, '4l1hXDOGHxbw70G52sPJwucyugk1uPufZZR3vm+Jillxese3kZrI99EekYSiWYuCOPOKJQoJn6bqyqwKgLPZDAE=', 0, 5789, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('NO/uMV7ehqq/gm1OtYVz5dcPaFK6wfNjhhomAlptwxo=', '2020-07-19 21:14:24', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '3ezRvF7FrfiFwPgS8GmHkw2cAEuM', null, 5, 'EpGKLtt+RURyRRf535yPuWA7Sh57nNU5t6BsTZocKRt3H6kD2lUWH79qlfjaNtcNRgPpiBZDkyUBosME6HXfYgA=', 0, 5788, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('6t6CgntayIFTlcnLNaqdKRJ9oCGiHkS2RuEdFg9+Qao=', '2020-07-19 21:14:11', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2AxxFU3W6L4eNuSsADf876cpYx7k', null, 48, 'vzA9d+ztLLHbIIaWfdWBK9MDT75bobKgQF3zu42d1S1VaOY8ejZfuKj1+n9z2tzuC3IvbTIKuVbKcgXnkAPFGwE=', 0, 5787, null,'Success', 'Transfer', 0);
+INSERT INTO likelib.transactions(hash, "timestamp", account_to, account_from, data, amount, sign, fee, block_height, message, status, type, fee_left) 
+    VALUES ('U3D0ksqtTZvhhuH0Cl8e70AyC2onsoSETFIVwjMXxbA=', '2020-07-19 21:13:55', '3Kw1Ap7cayQ7MS2f2akdAZKHx6BM', '2kdCcijCmHQBQeN88SHZpW8QKVJ', null, 24, 'JVdHfT3786ASx4pcUnc8YpT47wVIy/RwFz9aeZ37wChhNM022BBRCQZ90ucnm7koDHqFu3qYqqCgRS63pqtRmQA=', 0, 5786, null,'Success', 'Transfer', 0);
